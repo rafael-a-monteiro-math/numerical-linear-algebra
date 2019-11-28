c67fd1015243fb4802ed85e9c66794076f843cc8
Rafael Monteiro <rafaelmonteiro@dhcp047117.t.aist.go.jp>
2019-11-28 17:39:45 +0900

    second commit - short notes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stochastic-Simulations
+A glimpse of some stochastic simulation methods.  
+I don't claim originality for the results posted here: these are mostly some quick experiments I did while doing research. 
---
+++ build_site.R
@@ -0,0 +1,10 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/rafaelmonteiro/Dropbox/Matham-oil/R-markdown-website/Stochastic-Simulations")
+
+#render your sweet site. 
+rmarkdown::render_site()
+
+
